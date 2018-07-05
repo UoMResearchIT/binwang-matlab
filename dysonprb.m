5fc626e4349cfe32a0194997679c0da38a15ac6e
Gerard Capes <gerard.capes@manchester.ac.uk>
2018-07-05 07:58:37 +0100

    Use relative path to data file
    Also:
    - Don't use clear all
    - Remove comments using different character encoding

--- dysonprb.m
+++ dysonprb.m
@@ -1,7 +1,7 @@
 
-clear all
+clear
 clc
-data = load('C:\Users\Wangbin\Desktop\1.txt');
+data = load('1.txt');
 x = data(:,1);
 y = data(:,2);
 plot(x,y)
@@ -11,7 +11,7 @@ y = (x-A(2))/A(1); %b(2) H0; b(1) deta H
 Ydyson = @(A,x)(cos(A(3)*pi/180)/A(1)/A(1))*(-2*y+(1-y*y)*tan(A(3)*pi/180))/(1+y*y)/(1+y*y); 
 %is the function writing OK? or the expression approach of cos/tan function is correct and its value seating?
 
-A0 = [2 3509 10];  %≥ı º÷µ
+A0 = [2 3509 10]; 
 lA = [1 3485 0];
 uA = [40 3520 45];
 
